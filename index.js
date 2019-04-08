8dcbc7fb405be8aa5c3176d49c41f0309bafcc77
Ozberk Cetin <ozberkctn@gmail.com>
2019-04-08 10:19:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { PanResponder, View, TouchableHighlight, Animated, Text } from 'react-native';
+import React from "react";
+import {
+  PanResponder,
+  View,
+  TouchableHighlight,
+  Animated,
+  Text
+} from "react-native";
 
 class Switch extends React.Component {
   constructor(props) {
     super(props);
-    const w = this.props.switchWidth - Math.min(this.props.switchHeight, this.props.buttonRadius * 2);
+    const w =
+      this.props.switchWidth -
+      Math.min(this.props.switchHeight, this.props.buttonRadius * 2);
     this.state = {
       width: w,
-      state: this.props.active,
-      position: new Animated.Value(this.props.active ? w : 0),
+      position: new Animated.Value(this.props.active ? w : 0)
     };
     this.start = {};
   }
@@ -57,48 +64,58 @@ class Switch extends React.Component {
           }
         }
         let currentPos = this.state.position._value;
-        this.onSwipe(currentPos, this.start.pos,
+        this.onSwipe(
+          currentPos,
+          this.start.pos,
           () => {
             if (!this.start.state) this.start.stateChanged = true;
-            this.setState({ state: true })
+            this.setState({ state: true });
           },
           () => {
             if (this.start.state) this.start.stateChanged = true;
-            this.setState({ state: false })
-          });
+            this.setState({ state: false });
+          }
+        );
       },
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
         this.setState({ pressed: false });
         let currentPos = this.state.position._value;
-        if (!this.start.moved || (Math.abs(currentPos - this.start.pos) < 5 && !this.start.stateChanged)) {
+        if (
+          !this.start.moved ||
+          (Math.abs(currentPos - this.start.pos) < 5 &&
+            !this.start.stateChanged)
+        ) {
           this.toggle();
           return;
         }
-        this.onSwipe(currentPos, this.start.pos, this.activate, this.deactivate);
+        this.onSwipe(
+          currentPos,
+          this.start.pos,
+          this.activate,
+          this.deactivate
+        );
       },
       onPanResponderTerminate: (evt, gestureState) => {
         let currentPos = this.state.position._value;
         this.setState({ pressed: false });
-        this.onSwipe(currentPos, this.start.pos, this.activate, this.deactivate);
+        this.onSwipe(
+          currentPos,
+          this.start.pos,
+          this.activate,
+          this.deactivate
+        );
       },
-      onShouldBlockNativeResponder: (evt, gestureState) => true,
+      onShouldBlockNativeResponder: (evt, gestureState) => true
     });
   };
 
-  componentWillReceiveProps = (nextProps) => {
-    if (this.state.state !== nextProps.active) {
-      if (nextProps.active) {
-        this.activate();
-      } else {
-        this.deactivate();
-      }
-    }
-  }
-
   onSwipe = (currentPosition, startingPosition, onChange, onTerminate) => {
     if (currentPosition - startingPosition >= 0) {
-      if (currentPosition - startingPosition > this.state.width / 2 || startingPosition == this.state.width) {
+      if (
+        currentPosition - startingPosition > this.state.width / 2 ||
+        startingPosition == this.state.width
+      ) {
         onChange();
       } else {
         onTerminate();
@@ -113,28 +130,22 @@ class Switch extends React.Component {
   };
 
   activate = () => {
-    Animated.timing(
-      this.state.position,
-      {
-        toValue: this.state.width,
-        duration: this.props.switchAnimationTime,
-      }
-    ).start();
+    Animated.timing(this.state.position, {
+      toValue: this.state.width,
+      duration: this.props.switchAnimationTime
+    }).start();
     this.changeState(true);
   };
 
   deactivate = () => {
-    Animated.timing(
-      this.state.position,
-      {
-        toValue: 0,
-        duration: this.props.switchAnimationTime,
-      }
-    ).start();
+    Animated.timing(this.state.position, {
+      toValue: 0,
+      duration: this.props.switchAnimationTime
+    }).start();
     this.changeState(false);
   };
 
-  changeState = (state) => {
+  changeState = state => {
     let callHandlers = this.start.state != state;
     setTimeout(() => {
       this.setState({ state: state });
@@ -170,40 +181,65 @@ class Switch extends React.Component {
     return (
       <View
         {...this._panResponder.panHandlers}
-        style={{padding: this.props.padding, position: 'relative'}}>
+        style={{ padding: this.props.padding, position: "relative" }}
+      >
         <View
           style={{
-            backgroundColor: this.state.state ? this.props.activeBackgroundColor : this.props.inactiveBackgroundColor,
+            backgroundColor: this.state.state
+              ? this.props.activeBackgroundColor
+              : this.props.inactiveBackgroundColor,
             height: this.props.switchHeight,
             width: this.props.switchWidth,
-            borderRadius: this.props.switchHeight/2,
+            borderRadius: this.props.switchHeight / 2,
             borderWidth: this.props.borderWidth,
-            borderColor: this.props.borderColor,
-          }}/>
-        <TouchableHighlight underlayColor='transparent' activeOpacity={1} style={{
-            height: Math.max(this.props.buttonRadius*2+doublePadding, this.props.switchHeight+doublePadding),
-            width: this.props.switchWidth+doublePadding,
-            position: 'absolute',
+            borderColor: this.props.borderColor
+          }}
+        />
+        <TouchableHighlight
+          underlayColor="transparent"
+          activeOpacity={1}
+          style={{
+            height: Math.max(
+              this.props.buttonRadius * 2 + doublePadding,
+              this.props.switchHeight + doublePadding
+            ),
+            width: this.props.switchWidth + doublePadding,
+            position: "absolute",
             top: 1,
-            left: 1,
-          }}>
-          <Animated.View style={[{
-              backgroundColor:
-                this.state.state
-                  ? (this.state.pressed? this.props.activeButtonPressedColor : this.props.activeButtonColor)
-                  : (this.state.pressed? this.props.inactiveButtonPressedColor : this.props.inactiveButtonColor),
-              height: this.props.buttonRadius*2,
-              width: this.props.buttonRadius*2,
-              borderRadius: this.props.buttonRadius,
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexDirection: 'row',
-              position: 'absolute',
-              top: halfPadding + this.props.switchHeight/2 - this.props.buttonRadius,
-              left: this.props.switchHeight/2 > this.props.buttonRadius ? halfPadding : halfPadding + this.props.switchHeight/2 - this.props.buttonRadius,
-              transform: [{ translateX: this.state.position }]
-            },
-            this.props.buttonShadow]}
+            left: 1
+          }}
+        >
+          <Animated.View
+            style={[
+              {
+                backgroundColor: this.state.state
+                  ? this.state.pressed
+                    ? this.props.activeButtonPressedColor
+                    : this.props.activeButtonColor
+                  : this.state.pressed
+                  ? this.props.inactiveButtonPressedColor
+                  : this.props.inactiveButtonColor,
+                height: this.props.buttonRadius * 2,
+                width: this.props.buttonRadius * 2,
+                borderRadius: this.props.buttonRadius,
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "row",
+                position: "absolute",
+                top:
+                  halfPadding +
+                  this.props.switchHeight / 2 -
+                  this.props.buttonRadius,
+                left:
+                  this.props.switchHeight / 2 > this.props.buttonRadius
+                    ? halfPadding
+                    : halfPadding +
+                      this.props.switchHeight / 2 -
+                      this.props.buttonRadius,
+                transform: [{ translateX: this.state.position }]
+              },
+              this.props.buttonShadow
+            ]}
           >
             {this.props.buttonContent}
           </Animated.View>
@@ -216,18 +252,18 @@ Switch.defaultProps = {
   active: false,
   style: {},
   padding: 2,
-  inactiveButtonColor: '#2196F3',
-  inactiveButtonPressedColor: '#42A5F5',
-  activeButtonColor: '#FAFAFA',
-  activeButtonPressedColor: '#F5F5F5',
+  inactiveButtonColor: "#2196F3",
+  inactiveButtonPressedColor: "#42A5F5",
+  activeButtonColor: "#FAFAFA",
+  activeButtonPressedColor: "#F5F5F5",
   buttonShadow: {
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.5,
     shadowRadius: 1,
-    shadowOffset: { height: 1, width: 0 },
+    shadowOffset: { height: 1, width: 0 }
   },
-  activeBackgroundColor: 'rgba(255,255,255,.5)',
-  inactiveBackgroundColor: 'rgba(0,0,0,.5)',
+  activeBackgroundColor: "rgba(255,255,255,.5)",
+  inactiveBackgroundColor: "rgba(0,0,0,.5)",
   buttonRadius: 15,
   switchWidth: 40,
   switchHeight: 20,
@@ -235,13 +271,10 @@ Switch.defaultProps = {
   enableSlide: true,
   switchAnimationTime: 200,
   borderWidth: 1,
-  borderColor: '#ddd',
-  onActivate: () => {
-  },
-  onDeactivate: () => {
-  },
-  onChangeState: () => {
-  },
+  borderColor: "#ddd",
+  onActivate: () => {},
+  onDeactivate: () => {},
+  onChangeState: () => {}
 };
 
 export { Switch };
